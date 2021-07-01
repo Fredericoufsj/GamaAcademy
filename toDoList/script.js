61ce70e1997217aadf845ed7a5c3ac3a58592599
frederico <fredericoufsj1gmail.com>
2021-06-30 22:21:57 -0300

    first commit: html e css

---
+++ toDoList/script.js
@@ -0,0 +1,3 @@
+const listContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
