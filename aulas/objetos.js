c859cecee71965ba79996eb9d891cba2c4708e4b
frederico <fredericoufsj1gmail.com>
2021-07-03 22:58:20 -0300

    teste

---
+++ aulas/objetos.js
@@ -0,0 +1,17 @@
+const pessoa = {
+    nome: 'Frederico',
+    idade: 32,
+    cidade: 'Conselheiro lafaiete'
+}
+
+// Notação de ponto
+console.log(pessoa.nome)
+
+// Notação de colchetes
+console.log(pessoa['idade'])
+
+// Como desestruturar objetos
+const { nome, idade, cidade } = pessoa
+console.log(nome)
+console.log(idade)
+console.log(cidade)
